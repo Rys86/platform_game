211310ad03f01a03318d1c92d5a8ac7351c2efc3
Rys86 <turkowski.mariusz@gmail.com>
2024-11-19 23:50:07 +0100

    event Handler added

--- game.py
+++ game.py
@@ -1,5 +1,19 @@
 import pygame
+import sys
 
 pygame.init()
 
-screen = pygame.display.set_mode()
+# Create a window
+screen = pygame.display.set_mode((640,480))
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    pygame.display.update()
+    clock.tick(60)
