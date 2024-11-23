a7434bffe7d90eae668bdf89f9524ad69ceeb502
Rys86 <turkowski.mariusz@gmail.com>
2024-11-24 00:27:07 +0100

    some fixes after merging

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import pygame
 import sys
 
+
+from more_itertools.more import side_effect
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -9,14 +12,31 @@ class Game:
         self.screen = pygame.display.set_mode((640,480))
         self.clock = pygame.time.Clock()
         self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.img_pos = [160,250]
+        self.movement = [False,False]
+
 
     def run(self):
         while True:
+            self.screen.blit(self.img,self.img_pos)
+            self.img_pos[1] += self.movement[1]
             self.screen.blit(self.img,(100,200))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.KEYDOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.KEYDOWN:
+                        self.movement[1] = False
+
+
 
 
             pygame.display.update()
