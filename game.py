a7bf9b52009399947075381415041af5dd0ac254
Rys86 <turkowski.mariusz@gmail.com>
2024-12-08 16:03:26 +0100

    Particles animation added

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 import pygame
-
+import random
 import sys
+import math
 
 from scripts.utils import load_image, load_images, Animation
 from scripts.entities import PhysicsEntiti, Player
 from scripts.tilemap import Tilemap
 from scripts.clauds import Clauds
+from scripts.particle import Particle 
 
 
 class Game:
@@ -43,6 +45,7 @@ class Game:
             'player/jump': Animation(load_images('entities/player/jump')),
             'player/slide': Animation(load_images('entities/player/jump')),
             'player/wal_slide': Animation(load_images('entities/player/wall_slide')),
+            'particle/leaf': Animation(load_images('particles/leaf'),img_dur=20, loop=False),
         }
 
 
@@ -52,6 +55,13 @@ class Game:
 
         self.tilemap = Tilemap(self, tile_size=16)
         self.tilemap.load('map.json')
+
+        self.leaf_spawners = []
+        for tree in self.tilemap.extract([('large_decor', 2)], keep=True):
+            self.leaf_spawners.append(pygame.Rect(4 + tree['pos'][0], 4 + tree['pos'][1], 23, 13))
+        print(self.leaf_spawners)
+
+        self.particles = []
 
         self.scroll = [0,0]
 
@@ -66,6 +76,11 @@ class Game:
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
+            for rect in self.leaf_spawners:
+                if random.random() * 49999 < rect.width * rect.height:
+                    pos = (rect.x + random.random() * rect.width, rect.y + random.random() * rect.height)
+                    self.particles.append(Particle(self, 'leaf', pos, velocity=[-0.1,0.3], frame=random.randint(0,20)))
+
             self.clauds.update()
             self.clauds.render(self.display,offset=render_scroll)
 
@@ -73,6 +88,14 @@ class Game:
 
             self.player.update(self.tilemap,(self.movement[1] - self.movement[0], 0))
             self.player.render(self.display, offset=render_scroll)
+
+            for particle in self.particles.copy():
+                kill = particle.update()
+                particle.render(self.display, offset=render_scroll)
+                if particle.type == 'leaf':
+                    particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3
+                if kill:
+                    self.particles.remove(particle)
 
             # print(self.tilemap.tiles_around(self.player.pos))
 
