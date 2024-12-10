b342e4f967af0738ec6d233f543f499f75b4010f
Rys86 <turkowski.mariusz@gmail.com>
2024-12-10 22:14:16 +0100

    Dash Attack Efect added

--- game.py
+++ game.py
@@ -46,6 +46,8 @@ class Game:
             'player/slide': Animation(load_images('entities/player/jump')),
             'player/wall_slide': Animation(load_images('entities/player/wall_slide')),
             'particle/leaf': Animation(load_images('particles/leaf'),img_dur=20, loop=False),
+            'particle/particle': Animation(load_images('particles/particle'),img_dur=6, loop=False),
+
         }
 
 
@@ -79,7 +81,7 @@ class Game:
             for rect in self.leaf_spawners:
                 if random.random() * 49999 < rect.width * rect.height:
                     pos = (rect.x + random.random() * rect.width, rect.y + random.random() * rect.height)
-                    self.particles.append(Particle(self, 'leaf', pos, velocity=[-0.1,0.3], frame=random.randint(0,20)))
+                    self.particles.append(Particle(self, 'particle', pos, velocity=[-0.1,0.3], frame=random.randint(0,20)))
 
             self.clauds.update()
             self.clauds.render(self.display,offset=render_scroll)
@@ -110,6 +112,8 @@ class Game:
                         self.movement[1] = True
                     if event.key == pygame.K_UP:
                         self.player.jump()
+                    if event.key == pygame.K_x:
+                        self.player.dash()
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
